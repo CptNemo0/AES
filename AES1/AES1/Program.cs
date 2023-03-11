e10367d104aab7e0d165dbf31a3175f42654caca
242541 <242541@edu.p.lodz.pl>
2023-03-11 22:11:21 +0100

    encryption is working

--- AES1/AES1/Program.cs
+++ AES1/AES1/Program.cs
@@ -1,2 +1,374 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text;
+
+public struct word
+{
+    public byte[] w;
+    public word(byte b0, byte b1, byte b2, byte b3)
+    {
+        w = new byte[4];
+        w[0] = b0;
+        w[1] = b1;
+        w[2] = b2;
+        w[3] = b3;
+    }
+
+    public static word operator ^(word word1, word word2)
+    {
+        word1.w[0] = (byte)(word1.w[0] ^ word2.w[0]);
+        word1.w[1] = (byte)(word1.w[1] ^ word2.w[1]);
+        word1.w[2] = (byte)(word1.w[2] ^ word2.w[2]);
+        word1.w[3] = (byte)(word1.w[3] ^ word2.w[3]);
+        return word1;
+    }
+
+    public void print()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            byte[] tmp = new byte[1];
+            tmp[0] = w[i];
+            Console.Write(BitConverter.ToString(tmp));
+        }
+        Console.WriteLine();
+    }
+};
+
+public class AESCipher
+{
+    public readonly static byte[,] sbox = {
+                            { 0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76 },
+                            { 0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0 },
+                            { 0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15 },
+                            { 0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75 },
+                            { 0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84 },
+                            { 0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF },
+                            { 0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8 },
+                            { 0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2 },
+                            { 0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73 },
+                            { 0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB },
+                            { 0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79 },
+                            { 0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08 },
+                            { 0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A },
+                            { 0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E },
+                            { 0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF },
+                            { 0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16 }
+                        };
+
+    public static byte[] testKey_s = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+
+    public static byte[,] testInput_s = 
+    {
+        {0x32, 0x88, 0x31, 0xe0},
+        {0x43, 0x5a, 0x31, 0x37},
+        {0xf6, 0x30, 0x98, 0x07},
+        {0xa8, 0x8d, 0xa2, 0x34}
+    };
+
+    public static byte[] testKey2 = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+
+    public static byte[] testKey3 = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+
+    public static byte[] test_plaintext = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
+    
+
+    public static byte[] getCipherKey(int Nk)
+    {
+        int len = 0;
+
+        if (Nk == 4)
+        {
+            len = 128 / 8;
+        }
+        else if (Nk == 8)
+        {
+            len = 256 / 8;
+        }
+        else if (Nk == 6)
+        {
+            len = 192 / 8;
+        }
+        else
+        {
+            throw new Exception("Only 128, 192 and 256 are valid lengths!");
+        }
+
+        byte[] key = new byte[len]; // 16 bytes = 128 bits
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(key);
+        }
+        return key;
+    }
+
+    public static byte[] MessageInBinary(string message, Encoding encoding)
+    {
+        return encoding.GetBytes(message);
+    }
+
+    public static byte[] ConvertToByteArray(string str, Encoding encoding)
+    {
+        return encoding.GetBytes(str);
+    }
+
+    public static String ToBinary(Byte[] data)
+    {
+        return string.Join("", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+    }
+
+    public static byte[,] BlockToState(byte[] block)
+    {
+        byte[,] retValue = { { block[0], block[4], block[8], block[12] },
+                             { block[1], block[5], block[9], block[13] },
+                             { block[2], block[6], block[10], block[14] },
+                             { block[3], block[7], block[11], block[15] } };
+
+        return retValue;
+    }
+
+    public static byte[] StateToBlock(byte[,] state)
+    {
+        byte[] retValue = {
+                               state[0, 0], state[1, 0], state[2, 0], state[3, 0], state[0, 1], state[1, 1], state[2, 1], state[3, 1],
+                               state[0, 2], state[1, 2], state[2, 2], state[3, 2], state[0, 3], state[1, 3], state[2, 3], state[3, 3]
+                          };
+        return retValue;
+    }
+
+    //Substitution of singular bit according to S-box
+    public static byte GetSubstituteByte(byte s)
+    {
+        byte retValue = 0;                          // Byte retValue is literall byte - sequention of 8 binary values, in this case 0000 0000.
+                                                    // Keep in mind that single hexadecimal digit (form 0 to F) can ALWAYS be represented using 4 bits.
+                                                    // Thus 1 byte is comprised of 2 hexadeciaml numbers. 
+
+        byte ColumnIndex = (byte)(s & 0x0F);        // We are writting 4 youngest (first from right) bits of input byte, to the variable ColumnIndex. 
+                                                    // We are using logical AND operation on bites of input byte and 0x0F (0000 1111).
+                                                    // This way we are zeroing 4 oldest bits and preserving 4 youngest.
+        byte RowIndex = (byte)(s & 0xF0);           // Now we are accessing 4 oldest bits in the smae fashion. 
+                                                    // Keep in mind that result of that operation zeroes 4 youngest bits and leaves us with 16 times bigger number. 
+        RowIndex = (byte)(RowIndex >> 4);           // To combat this we are just simply bit-shiffting RowIndex 4 times to right. Which is equivalent to dividing by 2^4. 
+
+        retValue = sbox[RowIndex, ColumnIndex];     // We access sbox and return value that passed state should be substituted with.
+
+        return retValue;
+    }
+
+    //Transformation in the Cipher that processes the State
+    //using a nonlinear byte substitution table (S-box)
+    //that operates on each of the State bytes independently.
+    public static void SubBytes(ref byte[,] state)  
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                state[i, j] = GetSubstituteByte(state[i, j]);
+            }
+        }
+    }
+
+    public static byte GF256multiplication(byte a, byte b)
+    {
+        byte retValue = 0;
+        byte t;
+
+        while(a != 0)
+        { 
+            if((a & 1) != 0)
+            {
+                retValue = (byte)(retValue ^ b);
+            }
+            t = (byte)(b & 0x80);
+            b = (byte)(b << 1) ;
+            if(t != 0)
+            {
+                b = (byte)(b ^ 0x1b);
+            }
+            a = (byte)((a & 0xff) >> 1);
+        }
+
+        return retValue;
+    }
+
+    public static void ShiftRows(ref byte[,] state)
+    {
+        byte s0 = state[1, 0];
+        byte s1 = state[1, 1];
+        byte s2 = state[1, 2];
+        byte s3 = state[1, 3];
+
+        state[1, 0] = s1;
+        state[1, 1] = s2;
+        state[1, 2] = s3;
+        state[1, 3] = s0;
+
+        s0 = state[2, 0];
+        s1 = state[2, 1];
+        s2 = state[2, 2];
+        s3 = state[2, 3];
+
+        state[2, 0] = s2;
+        state[2, 1] = s3;
+        state[2, 2] = s0;
+        state[2, 3] = s1;
+
+        s0 = state[3, 0];
+        s1 = state[3, 1];
+        s2 = state[3, 2];
+        s3 = state[3, 3];
+
+        state[3, 0] = s3;
+        state[3, 1] = s0;
+        state[3, 2] = s1;
+        state[3, 3] = s2;
+    }
+
+    public static void MixColumns(ref byte[,] state)
+    {
+        int[] helper_array = new int[4];
+        for(int c = 0; c < 4; c++)
+        {
+            helper_array[0] = (GF256multiplication(0x02, state[0, c])) ^ (GF256multiplication(0x03, state[1, c])) ^ (state[2, c]) ^ (state[3, c]);
+            helper_array[1] = (state[0, c]) ^ (GF256multiplication(0x02, state[1, c])) ^ (GF256multiplication(0x03, state[2, c])) ^ (state[3, c]);
+            helper_array[2] = (state[0, c]) ^ (state[1, c]) ^ (GF256multiplication(0x02, state[2, c])) ^ (GF256multiplication(0x03, state[3, c]));
+            helper_array[3] = (GF256multiplication(0x03, state[0, c])) ^ (state[1, c]) ^ (state[2, c]) ^ (GF256multiplication(0x02, state[3, c]));
+            
+            for(int r = 0; r < 4; r++)
+            {
+                state[r, c] = (byte)(helper_array[r]);
+            }
+        }
+    }
+
+    public static void AddRoundKey(ref byte[,] state, word[] ks, int round)
+    {
+        int Nb = 4;
+        int[] helper_array = new int[4];
+        for (int c = 0; c < 4; c++)
+        {
+            helper_array[0] = (state[0, c] ^ ks[round * Nb + c].w[0]);
+            helper_array[1] = (state[1, c] ^ ks[round * Nb + c].w[1]);
+            helper_array[2] = (state[2, c] ^ ks[round * Nb + c].w[2]);
+            helper_array[3] = (state[3, c] ^ ks[round * Nb + c].w[3]);
+
+            for (int r = 0; r < 4; r++)
+            {
+                state[r, c] = (byte)(helper_array[r]);
+            }
+        }
+    }
+
+    public static word SubWord(word w)
+    {
+        return new word(GetSubstituteByte(w.w[0]), GetSubstituteByte(w.w[1]), GetSubstituteByte(w.w[2]), GetSubstituteByte(w.w[3]));
+    }
+
+    public static word RotWord(word w)
+    {
+        return new word(w.w[1], w.w[2], w.w[3], w.w[0]);
+    }
+
+    public static word Rcon(int i)
+    {
+        if (i < 9)
+        {
+            return new word((byte)(Math.Pow(0x02, i - 1)), (byte)0x00, (byte)0x00, (byte)0x00);
+        }
+        else if (i == 9)
+        {
+            return new word((byte)0x1b, (byte)0x00, (byte)0x00, (byte)0x00);
+        }
+        else if (i == 10)
+        {
+            return new word((byte)0x36, (byte)0x00, (byte)0x00, (byte)0x00);
+        }
+        else
+        {
+            return new word(0x00, 0x00, 0x00, 0x00);
+        }
+    }
+
+    public static word[] KeyExpansion(byte[] K)
+    {
+        int Nb = 4;
+        int Nk = K.Length / 4;
+        int Nr = Nb + Nk + 2;
+        word[] w = new word[Nb * (Nr + 1)];
+        word[] retValue = new word[Nb * (Nr + 1)];
+
+        for(int z = 0; z < w.Length; z++)
+        {
+            w[z] = new word(0x00, 0x00, 0x00, 0x00);
+            retValue[z] = new word(0x00, 0x00, 0x00, 0x00);
+        }
+        word temp;
+
+        int i = 0;
+
+        while (i < Nk)
+        {
+            w[i] = new word(K[4 * i], K[4 * i + 1], K[4 * i + 2], K[4 * i + 3]);
+            retValue[i] = new word(w[i].w[0], w[i].w[1], w[i].w[2], w[i].w[3]); //it works.
+            i++;
+        }
+
+        i = Nk;
+
+        while (i < Nb * (Nr + 1))
+        {
+            temp = w[i - 1];
+            if(i % Nk == 0)
+            {
+                temp = RotWord(temp);
+                temp = SubWord(temp);
+                temp = temp ^ Rcon(i/Nk);
+            }
+            else if(Nk > 6 && i % Nk == 4)
+            {
+                temp = SubWord(temp);
+            }
+            w[i] = w[i - Nk] ^ temp;
+            retValue[i] = new word(w[i].w[0], w[i].w[1], w[i].w[2], w[i].w[3]); //it works.
+            i++;
+        }
+
+        return retValue;
+    }
+
+    public static byte[,] Cipher(byte[,] input, word[] KeyScheadule, int Nk)
+    {
+        byte[,] state = input;
+        int Nb = 4;
+        int lastRound = 0;
+        AddRoundKey(ref state, KeyScheadule, 0);
+
+        for (int round = 1; round < (Nb + Nk + 2); round++)
+        {
+            SubBytes(ref state);
+            ShiftRows(ref state);
+            MixColumns(ref state);
+            AddRoundKey(ref state, KeyScheadule, round);
+            lastRound = round;
+        }
+
+        SubBytes(ref state);
+        ShiftRows(ref state);
+        AddRoundKey(ref state, KeyScheadule, lastRound + 1);
+        
+        return state;
+    }
+
+    public static void Main()
+    {
+        word[] KeyScheadule = KeyExpansion(testKey3);
+
+        byte[,] complete1 = Cipher(BlockToState(test_plaintext), KeyScheadule, 6);
+
+        byte[] output = StateToBlock(complete1);
+        Console.Write(BitConverter.ToString(output));
+    }
+}
